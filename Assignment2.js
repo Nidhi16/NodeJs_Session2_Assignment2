cfbd6187b6c499f3e21fe553e0e2c6e072d2e8c7
Nidhi16 <nidhi.valuable@gmail.com>
2017-10-21 21:22:20 +0530

    Made a closure which multiplies outer and inner variable

---
+++ Assignment2.js
@@ -0,0 +1,15 @@
+// An outerFunc function which has outerParam parameter
+var outerFunc = function(outerParam) {
+    // A closure function which uses variable of outerFunc function
+    var innerFunc = function(innerParam) {
+        var result = outerParam * innerParam;
+        console.log("Multiplication of outer parameter" + " (" + outerParam + ") " + "and inner parameter" + " (" + innerParam + ") " + "is: " + result);
+        alert("Multiplication of outer parameter" + " (" + outerParam + ") " + "and inner parameter" + " (" + innerParam + ") " + "is: " + result);
+    };
+    return innerFunc;
+};
+
+// calling outerFunc
+var closureFunction = outerFunc(4);
+// calling closure
+closureFunction(5);
